10b566b2d37f7ed292e17d05e6a2ef0807bd1a1a
Miniberry <dzigh30@gmail.com>
2024-02-04 20:37:43 +0900

    Moving our Platforms

--- app.js
+++ app.js
@@ -39,10 +39,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+                platform.bottom -= 4;
+                let visual = platform.visual;
+                visual.style.bottom = platform.bottom + 'px';
+            });
+        }
+    }
+
     function start() {
         if (!isGameOver){
             createDoodler();
             createPlatforms();
+            setInterval(movePlatforms, 30);
         }
     }
     //attach to button
